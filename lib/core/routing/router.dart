3e1d07137228560ddeba4717165c4805574e2547
saci zakaria <sacideveloppeur@gmail.com>
2024-02-26 15:18:42 +0100

    case Routes.splash:

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -1,10 +1,17 @@
+import 'package:ecomm_ahmedbbdo_v1/core/constants/string.dart';
+import 'package:ecomm_ahmedbbdo_v1/features/auth/view/splash.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
     final arguments = settings.arguments;
 
-    switch (settings.name) {}
+    switch (settings.name) {
+      case Routes.splash:
+        return MaterialPageRoute(
+          builder: (_) => const Splash(),
+        );
+    }
 
     return MaterialPageRoute(
       builder: (_) => const Text("data"),
