df960762cccaaf404c0463b5465f7ad21e16f193
saci zakaria <sacideveloppeur@gmail.com>
2024-02-26 15:29:44 +0100

    case Routes.onBoarding:

--- lib/core/routing/router.dart
+++ lib/core/routing/router.dart
@@ -2,15 +2,17 @@ import 'package:ecomm_ahmedbbdo_v1/core/constants/string.dart';
 import 'package:ecomm_ahmedbbdo_v1/features/auth/view/splash.dart';
 import 'package:flutter/material.dart';
 
+import '../../features/auth/view/onBoarding.dart';
+
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
     final arguments = settings.arguments;
 
     switch (settings.name) {
       case Routes.splash:
-        return MaterialPageRoute(
-          builder: (_) => const Splash(),
-        );
+        return MaterialPageRoute(builder: (_) => const Splash());
+      case Routes.onBoarding:
+        return MaterialPageRoute(builder: (_) => const OnBoarding());
     }
 
     return MaterialPageRoute(
