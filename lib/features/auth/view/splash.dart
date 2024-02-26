b071e291a3d9a5f5a8aea13d152394295fe6f477
saci zakaria <sacideveloppeur@gmail.com>
2024-02-26 15:41:50 +0100

    Image.asset("assets/images/Logo_Splash_Screen.png"),

--- lib/features/auth/view/splash.dart
+++ lib/features/auth/view/splash.dart
@@ -10,8 +10,9 @@ class Splash extends StatefulWidget {
 class _SplashState extends State<Splash> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color.fromARGB(26, 239, 250, 194),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(26, 239, 250, 194),
+      body: Image.asset("assets/images/Logo_Splash_Screen.png"),
     );
   }
 }
