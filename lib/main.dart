50f802304b0a695bc28cf4883314a97753efb3cf
saci zakaria <sacideveloppeur@gmail.com>
2024-02-27 14:25:15 +0100

    initialRoute: Routes.splash,

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         ),
         onGenerateRoute: appRouter.generateRoute,
         //home: const Splash(),
-        initialRoute: Routes.onBoarding,
+        initialRoute: Routes.splash,
       ),
     );
   }
