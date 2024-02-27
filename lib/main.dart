0f796d8b033c9d2c1458ff72566a3fb0a9dd6c56
saci zakaria <sacideveloppeur@gmail.com>
2024-02-27 14:17:59 +0100

    designSize: const Size(375, 812),

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
+      designSize: const Size(375, 812),
       child: MaterialApp(
         title: 'Marketic',
         theme: ThemeData(
