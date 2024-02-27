3161dcaa42a9f1f5f63734ca0d098d13f02e2bd7
saci zakaria <sacideveloppeur@gmail.com>
2024-02-27 14:19:10 +0100

    minTextAdapt: true,

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(375, 812),
+      minTextAdapt: true,
       child: MaterialApp(
         title: 'Marketic',
         theme: ThemeData(
