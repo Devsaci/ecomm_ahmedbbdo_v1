96a030beba49a35c064d9c4205abb186a1a9555b
saci zakaria <sacideveloppeur@gmail.com>
2024-02-28 19:55:43 +0100

    List<OnBordingModel> onBordingList = [];

---
+++ lib/core/utils/onbording_list.dart
@@ -0,0 +1,5 @@
+//import 'package:ecomm_ahmedbbdo_v1/features/onbording/data\model/onbording_model.dart';
+
+import '../../features/onbording/data/model/onbording_model.dart';
+
+List<OnBordingModel> onBordingList = [];
--- lib/features/onbording/data/model/onbording_model.dart
+++ lib/features/onbording/data/model/onbording_model.dart
@@ -1,8 +1,8 @@
-class OnbordingModel {
+class OnBordingModel {
   final String? image;
   final String? title;
   final String? testbody;
-  OnbordingModel({
+  OnBordingModel({
     required this.image,
     required this.title,
     required this.testbody,
