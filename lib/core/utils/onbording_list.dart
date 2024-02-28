40b503ed9bfb356e08d90ede7585a95077b95742
saci zakaria <sacideveloppeur@gmail.com>
2024-02-28 19:58:13 +0100

    OnBordingModel [ 1,2,3}

--- lib/core/utils/onbording_list.dart
+++ lib/core/utils/onbording_list.dart
@@ -2,4 +2,20 @@
 
 import '../../features/onbording/data/model/onbording_model.dart';
 
-List<OnBordingModel> onBordingList = [];
+List<OnBordingModel> onBordingList = [
+  OnBordingModel(
+    image: "assets/images/onbording1.png",
+    title: "On Boarding 1",
+    testbody: "On Boarding 1",
+  ),
+  OnBordingModel(
+    image: "assets/images/onbording2.png",
+    title: "On Boarding 2",
+    testbody: "On Boarding 2",
+  ),
+  OnBordingModel(
+    image: "assets/images/onbording3.png",
+    title: "On Boarding 3",
+    testbody: "On Boarding 3",
+  ),
+];
