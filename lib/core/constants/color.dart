0522adc1991aecfda507ad558714e32cf268a470
saci zakaria <sacideveloppeur@gmail.com>
2024-02-28 19:18:54 +0100

    static const darkBlue700

--- lib/core/constants/color.dart
+++ lib/core/constants/color.dart
@@ -2,5 +2,6 @@ import 'package:flutter/material.dart';
 
 class ApppColor {
   static const primaryColor = Color(0xff3F80FF);
-  static const lightBlue = Color(0xff659AFF);
+  static const lightBlue700 = Color(0xffB2CCFF);
+  static const darkBlue700 = Color(0xff002B7F);
 }
