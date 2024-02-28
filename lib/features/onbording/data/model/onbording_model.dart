49d377f7963e3d20cc2b4666f1c893e45cb2e0b9
saci zakaria <sacideveloppeur@gmail.com>
2024-02-28 19:45:09 +0100

    final String? testbody;

--- lib/features/onbording/data/model/onbording_model.dart
+++ lib/features/onbording/data/model/onbording_model.dart
@@ -1,9 +1,10 @@
 class OnbordingModel {
   final String image;
   final String title;
-
+  final String? testbody;
   OnbordingModel({
     required this.image,
     required this.title,
+    required this.testbody,
   });
 }
