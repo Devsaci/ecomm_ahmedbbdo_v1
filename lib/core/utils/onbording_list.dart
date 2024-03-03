ae886023793f6636b5c370cb9a25b334831e39ce
saci zakaria <sacideveloppeur@gmail.com>
2024-03-03 16:20:56 +0100

    String text ,textbody,image

--- lib/core/utils/onbording_list.dart
+++ lib/core/utils/onbording_list.dart
@@ -5,17 +5,20 @@ import '../../features/onbording/data/model/onbording_model.dart';
 List<OnBordingModel> onBordingList = [
   OnBordingModel(
     image: "assets/images/onbording1.png",
-    title: "On Boarding 1",
-    testbody: "On Boarding 1",
+    title: "Welcom to Marketi",
+    testbody:
+        "Discover a world of endless possibilities and shop from the comfort of your fingertips Browse through a wide range of products, from fashion and electronics to home.",
   ),
   OnBordingModel(
     image: "assets/images/onbording2.png",
-    title: "On Boarding 2",
-    testbody: "On Boarding 2",
+    title: "Easy to Buy",
+    testbody:
+        "Find the perfect item that suits your style and needs With secure payment options and fast delivery, shopping has never been easier.",
   ),
   OnBordingModel(
     image: "assets/images/onbording3.png",
-    title: "On Boarding 3",
-    testbody: "On Boarding 3",
+    title: "Wonderful User Experience",
+    testbody:
+        "Start exploring now and experience the convenience of online shopping at its best.",
   ),
 ];
