f8f63b06304afcfbdc0a6ab1b63cbb4a0f1ecac5
saci zakaria <sacideveloppeur@gmail.com>
2024-03-04 18:39:47 +0100

    backgroundColor: Colors.white,

--- lib/features/onbording/view/onBoarding.dart
+++ lib/features/onbording/view/onBoarding.dart
@@ -8,6 +8,7 @@ class OnBoarding extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
+      backgroundColor: Colors.white,
       body: Center(child: Text('OnBoarding')),
     );
   }
