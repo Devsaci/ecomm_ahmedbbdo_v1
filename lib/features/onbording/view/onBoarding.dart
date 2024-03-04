06f4050d84e4a6decc93e182905aff20e9900009
saci zakaria <sacideveloppeur@gmail.com>
2024-03-04 19:15:16 +0100

    PageView.builder

--- lib/features/onbording/view/onBoarding.dart
+++ lib/features/onbording/view/onBoarding.dart
@@ -1,19 +1,28 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class OnBoarding extends StatelessWidget {
   const OnBoarding({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
         child: SizedBox(
           width: double.infinity,
           child: Column(
-            children: [],
+            children: <Widget>[
+              Expanded(
+                child: PageView.builder(
+                  itemBuilder: (context, index) {
+                    return const Text('OnBoarding');
+                  },
+                ),
+              ),
+            ],
           ),
         ),
       ),
